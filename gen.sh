08fdb2119554d0ba3b84112b2bdddea43e900b7c
Chen Yufei <cyfdecyf@gmail.com>
2022-03-17 09:30:46 +0800

    Fix main and gen.sh

--- gen.sh
+++ gen.sh
@@ -6,5 +6,5 @@ cmake --build build
 objdump -d build/e > dis/e.s
 objdump -d build/e_pic > dis/e_pic.s
 
-diff -u dis/e.s dis/e_pic.s > dis/diffu
+diff -u dis/e.s dis/e_pic.s > dis/diffu.patch
 
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "foo.h"
 
-int main(int argc, char** argv[]) {
+int main(int argc, char** argv) {
   return ml_func(1, 2);
 }
